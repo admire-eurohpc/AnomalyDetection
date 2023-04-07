122fad1ef556b2b18d32f63a23650312dff4ee83
Ignacy Stepka <ignacys@man.poznan.pl>
2023-04-07 15:53:21 +0200

    Add model evaluation script

---
+++ admire/models/ae_eval_model.py
@@ -0,0 +1,144 @@
+import logging
+import os
+import yaml
+from torch import nn
+import torch
+import pandas as pd
+import numpy as np
+import tqdm
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+from ae_litmodel import LitAutoEncoder
+from ae_dataloader import TimeSeriesDataset
+from utils.plotting import *
+LOGS_PATH = 'lightning_logs/ae/500nodes_2023_04_07-12_46_30'
+path = os.path.join(os.getcwd(), LOGS_PATH, 'checkpoints')
+
+print(os.path.exists(path))
+
+filenames = os.walk(path).__next__()[2] # get any checkpoint
+last_epoch_idx = np.array([int(i.split('-')[0].split('=')[1]) for i in filenames]).argmax()
+filename = filenames[last_epoch_idx]
+
+print(filename)
+
+
+checkpoint = os.path.join(path, filename)
+
+with open(os.path.join(os.getcwd(), LOGS_PATH, 'hparams.yaml'), 'r') as f:
+    params = yaml.load(f, yaml.UnsafeLoader)
+
+logging.basicConfig(level=logging.DEBUG)
+
+print(params)
+channels = 3
+height = params['number_of_nodes']
+width = params['window_size']
+input_shape = (channels, height, width)
+
+ENCODER_LAYERS = params['encoder_layers']
+DECODER_LAYERS = params['decoder_layers']
+LATENT_DIM = params['latent_dim']
+TEST_SLIDE = params['test_slide']
+WINDOW_SIZE = params['window_size']
+TRAIN_SLIDE = params['train_slide']
+
+
+use_cuda = torch.cuda.is_available()
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+train_dataset = TimeSeriesDataset(data_dir="data/processed/train/", 
+                                normalize=True, 
+                                window_size=WINDOW_SIZE, 
+                                slide_length=TRAIN_SLIDE)
+# Setup test data
+test_dataset = TimeSeriesDataset(data_dir="data/processed/test/", 
+                                    normalize=True, 
+                                    external_transform=train_dataset.get_transform(), # Use same transform as for training
+                                    window_size=WINDOW_SIZE, 
+                                    slide_length=TEST_SLIDE)
+test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, drop_last=True, **kwargs)
+
+# BUILD ENCODER
+encoder = nn.Sequential()
+encoder.append(nn.Linear(channels * width * height, ENCODER_LAYERS[0]))
+encoder.append(nn.GELU())
+
+for i in range(1, len(ENCODER_LAYERS)):
+    encoder.append(nn.Linear(ENCODER_LAYERS[i-1], ENCODER_LAYERS[i]))
+    encoder.append(nn.GELU())
+    
+encoder.append(nn.Linear(ENCODER_LAYERS[-1], LATENT_DIM))
+logging.debug(f'Encoder Summary: {encoder}')
+
+# BUILD DECODER
+decoder = nn.Sequential()
+decoder.append(nn.Linear(LATENT_DIM, DECODER_LAYERS[0]))
+decoder.append(nn.GELU())
+
+for i in range(1, len(DECODER_LAYERS)):
+    decoder.append(nn.Linear(DECODER_LAYERS[i-1], DECODER_LAYERS[i]))
+    decoder.append(nn.GELU())
+    
+decoder.append(nn.Linear(DECODER_LAYERS[-1], channels * width * height))
+logging.debug(f'Decoder Summary: {decoder}')
+
+
+autoencoder = LitAutoEncoder.load_from_checkpoint(
+                            checkpoint, 
+                            encoder=encoder, 
+                            decoder=decoder,
+                            input_shape=input_shape,
+                            latent_dim=LATENT_DIM
+                            )
+    
+# ----- TEST ----- #
+
+# Now test the model on february data
+# Run the model on the entire test set and report reconstruction error to tensorboard
+autoencoder.eval()
+autoencoder.freeze()
+
+logging.debug(f"Running model on test set")
+
+test_date_range = test_dataset.get_dates_range()
+test_date_range = pd.date_range(start=test_date_range['start'], end=test_date_range['end'], freq=f'{TEST_SLIDE}min', tz='Europe/Warsaw') # TODO set frequency dynamically?
+
+logging.debug(f'Dates range test: start {test_date_range.min()} end {test_date_range.max()}')
+logging.debug(f'Test len: {len(test_dataloader)}, Dates len: {len(test_date_range)}')
+
+test_reconstruction_mean_absolute_error = []
+for idx, batch in tqdm.tqdm(enumerate(test_dataloader), desc="Running test reconstruction error evaluation", total=len(test_dataloader)):
+    err = torch.sum(torch.abs(batch - autoencoder.decoder(autoencoder.encoder(batch)))).detach().numpy()
+    test_reconstruction_mean_absolute_error.append(err)
+
+    if idx == 100:
+        break
+
+# Display the reconstruction error over time manually
+logging.debug(f'Test reconstruction instances: {len(test_reconstruction_mean_absolute_error)}')
+
+logging.debug(f"Plotting reconstruction error over time")
+
+figure, ax = plt.subplots(1,1, figsize=(10, 8))
+ax.scatter(test_date_range.to_numpy()[:len(test_reconstruction_mean_absolute_error)], test_reconstruction_mean_absolute_error)
+ax.set_xlabel('Date')
+ax.set_ylabel('Reconstruction error')
+ax.set_title('Reconstruction error (test data)\nDate on x-axis corresponds to the *start* of the window')
+plt.xticks(rotation=70)
+plt.tight_layout()
+
+savedir = os.path.join(os.getcwd(), 'images', 'reconstruction')
+if not os.path.exists(savedir):
+    os.makedirs(savedir)
+
+plt.savefig(os.path.join(savedir, 'plt_reconstruction.png'))
+
+print('Plotly')
+
+plot_reconstruction_error_over_time(test_reconstruction_mean_absolute_error, 
+                                    test_date_range.to_numpy()[:len(test_reconstruction_mean_absolute_error)], 
+                                    show=False, 
+                                    write=True,
+                                    savedir=savedir)
--- admire/models/utils/plotting.py
+++ admire/models/utils/plotting.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List
 import numpy.typing as npt
 import plotly.express as px
 import plotly.io as pio
@@ -16,18 +16,18 @@ def plot_embeddings_vs_real(_embeddings: npt.NDArray,
         '''
         Plots the embeddings vs the real data.
         
-        `_embeddings` is a numpy array of shape (batch_size, channels, height, width)
-        `_real` is a numpy array of shape (batch_size, channels, height, width)
+        `_embeddings` is a numpy array of shape (channels, height, width)
+        `_real` is a numpy array of shape (channels, height, width)
         `
     
         '''
        
         pio.renderers.default = "browser"
        
-        _embeddings = _embeddings.reshape(-1, channels, height, width)
-        _real = _real.reshape(-1, channels, height, width)
+        _embeddings = _embeddings.reshape(channels, height, width)
+        _real = _real.reshape(channels, height, width)
         
-        time_dim = [i for i in range(_embeddings.shape[3])]
+        time_dim = [i for i in range(_embeddings.shape[2])]
         
         indices = [0, 1, 2, 3, 10, 11, 12, 13, 14] # 9 random indices
         channel = ['power', 'cpu1', 'cpu2']
@@ -37,11 +37,11 @@ def plot_embeddings_vs_real(_embeddings: npt.NDArray,
             fig = make_subplots(rows=3, cols=3)
             for i, idx in enumerate(indices):
                 
-                fig.add_scatter(x=time_dim, y=_embeddings[idx][c][height_node].astype(float), 
+                fig.add_scatter(x=time_dim, y=_embeddings[c, height_node, :].astype(float), 
                                 mode='lines+markers', name='Reconstructed',  
                                 line = dict(color='royalblue', width=4, dash='dash'), row=i % 3 + 1, col=i // 3 + 1)
                 
-                fig.add_scatter(x=time_dim, y=_real[idx][c][height_node].astype(float), 
+                fig.add_scatter(x=time_dim, y=_real[c, height_node, :].astype(float), 
                                 mode='lines+markers', name='Real',  
                                 line = dict(color='red', width=4, dash='dot'), row=i % 3 + 1, col=i // 3 + 1)
                 
@@ -63,14 +63,20 @@ def plot_embeddings_vs_real(_embeddings: npt.NDArray,
             
             
 def plot_reconstruction_error_over_time(reconstruction_errors: List[float],
-                                        time_axis: List[int] | None = None) -> None:
+                                        time_axis: List[int] | List[Any] | None = None,
+                                        show: bool = False,
+                                        write: bool = False,
+                                        savedir: bool = 'images'
+                                        ) -> None:
     '''
     Plots the reconstruction error over time.
     
     `reconstruction_errors` is a list of floats, where each float is the reconstruction error for a single timestep.
     `time_axis` is a list of ints, where each int is the timestep for the corresponding reconstruction error.
+    `show` set to `True` if the plot should be shown
+    `write` set to `True` if the plot should be saved to file
+    `savedir` directory in which the plot files will be saved
     '''
-    pio.renderers.default = "browser"
     
     if time_axis is None:
         time_axis = [i for i in range(len(reconstruction_errors))]
@@ -80,4 +86,11 @@ def plot_reconstruction_error_over_time(reconstruction_errors: List[float],
     fig.update_layout(
         title=f"Reconstruction error over time",
     )
-    fig.show()
+
+    if show:
+        pio.renderers.default = "browser"   
+        fig.show()
+
+    if write:
+        fig.write_html(os.path.join(savedir, 'plotly_reconstruction.html'))
+        fig.write_image(os.path.join(savedir, 'plotly_reconstruction.png'))
